df9c2488c5b54f9a73da61d0fa02c72f6325ed5b
PurnavPandit <77737419+PurnavPandit@users.noreply.github.com>
2021-05-14 16:16:12 +0530

    Add files via upload

---
+++ kwitterhw.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDZKOqhyszVgvA2cCDcYxrv5viYl6q9EwE",
+    authDomain: "patient-and-doctor-d4c76.firebaseapp.com",
+    databaseURL: "https://patient-and-doctor-d4c76.firebaseio.com",
+    projectId: "patient-and-doctor-d4c76",
+    storageBucket: "patient-and-doctor-d4c76.appspot.com",
+    messagingSenderId: "155228840695",
+    appId: "1:155228840695:web:059a009e780f3e76ee29f2"
+  };
+
+  firebase.initializeApp(firebaseConfig);
